756083dd718cd2aff6909a7b1612c708f9f14848
Daniel Gesua <danielgesua@gmail.com>
2020-06-06 00:42:57 -0700

    - Converted Gui into a mixin. - Converted repo into a package. - Transfered all gui code and import statements into their own gui   module. - added the new pycache to gitignore

--- access_db_exporter.py
+++ access_db_exporter.py
@@ -1,15 +1,13 @@
 import win32com.client
 import os
 import sys
-import tkinter
 import json
 import http.client
 import mimetypes
 import traceback
 from urllib.parse import quote
 from enum import IntFlag
-from tkinter.filedialog import askopenfilename
-from tkinter.messagebox import askyesno
+from gui.gui import GuiMixin
 
 class ms_access_automation():
     '''Object that uses COM to communicate with MS Access to get all the code from its modules and tabulate it in a python list.'''
@@ -386,90 +384,14 @@ class file_export_automation():
         _save_all_queries()
         _save_all_tables()
 
-class gui():
-    '''Validating inputs and (if necessary) opening a file dialog to request a valid MS Access file.'''
-
-    def _file_is_valid(self):
-        '''Checks to see if the file exists and is of the right extension. Returns true for "valid" and false otherwise'''
-
-        # If the file exists and is of appropriate extension then return true otherwise false.
-        if os.path.exists(self.db_path):
-            response = (os.path.splitext(self.db_path)[1] in ['.accdb'])
-        else:
-            response = False
-
-        return response 
-
-    def ask_for_db_path(self):
-        '''Asks the user for a path to the MS Access file, checks if it's valid, then--if it isn't--prompts the user for retry.'''
-
-        def _show_file_dialog_to_get_db_path():
-            '''Displays the file dialog that gets the MS Access file's path.'''
-            self.db_path = askopenfilename(
-                parent = self.window,
-                title = 'Pick MS Access database to export.',
-                filetypes = [('MS Office Databases', '*.accdb',)]
-                )
-
-        def _confirm_if_user_wants_to_retry():
-            '''Gets user confirmation to retry the file selection.'''
-            
-            nonlocal retry
-
-            # Build the user console prompt based on scenario:
-            #   Scenario #1) An invalid file name provided. Prompt should say the file was invalid.
-            #   Scenario #2) The file name was empty (cancel or close button on the dialog). Prompt should say no file selected.
-            user_prompt = 'The file "' + self.db_path + '" is not valid. ' if self.db_path != '' else 'No file selected. '
-            user_prompt += 'Would you like to try again?'
-
-            # Select the dialog's icon based on the scenario:
-            #   Scenario #1) Invalid file name. Display an error icon.
-            #   Scenario #2) Empty file name. Display a waring icon.
-            msgbox_icon = 'warning' if self.db_path == '' else 'error'
-
-            # Prompt the user for retry confirmation. and set the non-local variable.
-            retry = askyesno(
-                parent = self.window,
-                title = 'Invalid file.',
-                message = user_prompt,
-                icon = msgbox_icon
-            )
-
-        def _create_main_window():
-            '''Creates the main window obj and hides it.'''
-            self.window = tkinter.Tk()
-            self.window.withdraw()
-    
-        # Create the main GUI window.
-        _create_main_window()
-
-        # No need to retry if we get it right the first time.
-        retry = False
-
-        # Show a file dialog to the user so they can pick an Accdb.
-        _show_file_dialog_to_get_db_path()
-        
-        # If the file is not valid confirm if the user would like to try their choice again.
-        if not self._file_is_valid(): _confirm_if_user_wants_to_retry()
-
-        # If the file was valid or the user wishes to try again, then return to the begining of this routine.
-        if retry: self.ask_for_db_path()    
-
-    def __init__(self):
-        self.window = None
-
-    def __del__(self):
-        '''Closes the window when the program ends.'''
-        if self.window is not None: self.window.destroy()
-
-class automation(ms_access_automation, file_export_automation, gui):
+class automation(ms_access_automation, file_export_automation, GuiMixin):
     '''Object that performs all the automations necessary to export the modules in an access database.'''
 
     def __init__(self):
         '''Run all the base object initiators in the appropriate order.'''
         ms_access_automation.__init__(self)
         file_export_automation.__init__(self)
-        gui.__init__(self)
+        GuiMixin.__init__(self)
                 
     def run(self, db_path = '', pretty_print_sql = False):
         '''Runs the automation on a given path.'''
@@ -503,7 +425,7 @@ class automation(ms_access_automation, file_export_automation, gui):
     def __del__(self):
         '''Performs all the neccesary cleanup processes and closes the files.'''
         ms_access_automation.__del__(self)
-        gui.__del__(self)
+        GuiMixin.__del__(self)
 
 try:
     a = automation()
---
+++ gui/gui.py
@@ -0,0 +1,81 @@
+import tkinter
+import os
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import askyesno
+from abc import ABC
+
+class GuiMixin(ABC):
+    '''Validating inputs and (if necessary) opening a file dialog to request a valid MS Access file.'''
+
+    def _file_is_valid(self):
+        '''Checks to see if the file exists and is of the right extension. Returns true for "valid" and false otherwise'''
+
+        # If the file exists and is of appropriate extension then return true otherwise false.
+        if os.path.exists(self.db_path):
+            response = (os.path.splitext(self.db_path)[1] in ['.accdb'])
+        else:
+            response = False
+
+        return response 
+
+    def ask_for_db_path(self):
+        '''Asks the user for a path to the MS Access file, checks if it's valid, then--if it isn't--prompts the user for retry.'''
+
+        def _show_file_dialog_to_get_db_path():
+            '''Displays the file dialog that gets the MS Access file's path.'''
+            self.db_path = askopenfilename(
+                parent = self.window,
+                title = 'Pick MS Access database to export.',
+                filetypes = [('MS Office Databases', '*.accdb',)]
+                )
+
+        def _confirm_if_user_wants_to_retry():
+            '''Gets user confirmation to retry the file selection.'''
+            
+            nonlocal retry
+
+            # Build the user console prompt based on scenario:
+            #   Scenario #1) An invalid file name provided. Prompt should say the file was invalid.
+            #   Scenario #2) The file name was empty (cancel or close button on the dialog). Prompt should say no file selected.
+            user_prompt = 'The file "' + self.db_path + '" is not valid. ' if self.db_path != '' else 'No file selected. '
+            user_prompt += 'Would you like to try again?'
+
+            # Select the dialog's icon based on the scenario:
+            #   Scenario #1) Invalid file name. Display an error icon.
+            #   Scenario #2) Empty file name. Display a waring icon.
+            msgbox_icon = 'warning' if self.db_path == '' else 'error'
+
+            # Prompt the user for retry confirmation. and set the non-local variable.
+            retry = askyesno(
+                parent = self.window,
+                title = 'Invalid file.',
+                message = user_prompt,
+                icon = msgbox_icon
+            )
+
+        def _create_main_window():
+            '''Creates the main window obj and hides it.'''
+            self.window = tkinter.Tk()
+            self.window.withdraw()
+    
+        # Create the main GUI window.
+        _create_main_window()
+
+        # No need to retry if we get it right the first time.
+        retry = False
+
+        # Show a file dialog to the user so they can pick an Accdb.
+        _show_file_dialog_to_get_db_path()
+        
+        # If the file is not valid confirm if the user would like to try their choice again.
+        if not self._file_is_valid(): _confirm_if_user_wants_to_retry()
+
+        # If the file was valid or the user wishes to try again, then return to the begining of this routine.
+        if retry: self.ask_for_db_path()    
+
+    def __init__(self):
+        self.window = None
+
+    def __del__(self):
+        '''Closes the window when the program ends.'''
+        if self.window is not None: self.window.destroy()
